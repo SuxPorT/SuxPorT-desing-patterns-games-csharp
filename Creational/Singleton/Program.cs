9935f889e9acdc476919bf8e1fa8d7e2f9d1641c
Alexys Santiago <SuxPorT@hotmail.com>
2023-06-16 19:19:01 -0300

    feat: add Singleton pattern

---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Creational.Singleton
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var playerOne = Singleton.GetInstance;
+            playerOne.Message("Player 1: The ball is with me in midfield");
+
+            var playerTwo = Singleton.GetInstance;
+            playerTwo.Message("Player 2: Received the ball");
+
+            var playerThree = Singleton.GetInstance;
+            playerThree.Message("Player 3: received the launch at the bottom line");
+        }
+    }
+}
